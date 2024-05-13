277d3785c26fcdd42b38390703a115a465dbf164
Janik Stracke <j.stracke@itsmind.com>
2024-05-13 18:28:21 +0200

    chore: Test update

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,8 +6,8 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-parent_path=$( cd $(dirname "$0") ; pwd -P )
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 curl -s https://api.sevdesk.de/openapi.yaml -o external/openapi-updated.yaml
 
-"$parent_path/merge.sh" "external/openapi-updated.yaml"
+"$SCRIPT_DIR/merge.sh" "external/openapi-updated.yaml"
